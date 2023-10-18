401d317dfcd7f9e11e6cd05fdd9a10af4790223f
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-10-18 20:39:23 +0200

    code refactoring

--- _5.String_functions2.c
+++ _5.String_functions2.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 /**
  *_String_Length - Function to calculate length of the string
- *@s: string
+ *@string: string
  *Return: the length os string
  */
-int _String_Length(char *s)
+int _String_Length(char *string)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; string[i] != '\0'; i++)
 	{
 		;
 	}
@@ -17,24 +17,24 @@ int _String_Length(char *s)
 
 /**
  *_strncmp - Function to compare two strings
- *@s1: 1st string to comparate
- *@s2: 2nd string to comparate
- *@n: the number of characters to comparate
+ *@string1: 1st string to comparate
+ *@string2: 2nd string to comparate
+ *@number: the number of characters to comparate
  *Return: 0 is success o -1 is fail
  */
 
-int _strncmp(char *s1, char *s2, int n)
+int _strncmp(char *string1, char *string2, int number)
 {
 
 	int i = 0;
 
 	do {
-		if (s1[i] != s2[i])
+		if (string1[i] != string2[i])
 			return (-1);
 
 		i++;
 
-	} while (i != n);
+	} while (i != number);
 
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -4,6 +4,7 @@
  * Header File - shell.h
  * Authors: Siphephelo Khanyile
  */
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -12,34 +13,34 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <sys/stat.h>
-#include <limits.h>
-#define CHANGE_DIR 1
-#define EXIT_SHELL 2
 #define PERM_DENIED "Permission denied"
 #define NOT_FOUND "not found"
+#define CHANGE_DIR 1
+#define EXIT_SHELL 2
+
+int Putchar_(char c);
+int built_exit(char *command, int status);
+int wait_(char **argv, char *command, int QExecutes);
+int Exec_(char **param, char **env);
 int Atoi_(char *s);
 void rmvSpaces(char *str);
 void RvsString(char *str, int len);
 int IntegerToString(int x, char str[]);
 void Puts_(char *str);
-void _Errors(char *program, char *param, char *message, int Qexe);
 void Into_Hsh(char **env, char **argv);
 char **parse_command(char *command, char *separator);
 void Prompt_(void);
-int Exec_(char **param, char **env);
 void Print_Environment(char **env);
 int built_in(char *command, char **env, int status);
 char *Get_Environment(char *var, char **env);
 int Get_Path(char **param, char **env);
+char *_String_Conc(char *dest, char *src);
 void Execute_(char *command, char **env);
 char *_String_copy(char *dest, char *src);
-char *_String_Conc(char *dest, char *src);
 char *_String_Dupl(char *str);
 char *_String_Char(char *s, char c);
 int _String_Comp(char *s1, char *s2);
-int wait_(char **argv, char *command, int QExecutes);
-int built_exit(char *command, int status);
 int _String_Length(char *s);
 int _strncmp(char *s1, char *s2, int n);
-int Putchar_(char c);
+void _Errors(char *program, char *param, char *message, int Qexe);
 #endif
